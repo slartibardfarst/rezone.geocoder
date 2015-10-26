ab53e44a940c54e24293dcbc6d933c8794789d91
Andrew Watkins <andrew.watkins@move.com>
2015-10-26 12:21:56 -0700

    fixed bugs in pattern generation code

--- rezone.geocoder.parser/src/main/java/rezone/geocoder/parser/patterns/ProductionRule.java
+++ rezone.geocoder.parser/src/main/java/rezone/geocoder/parser/patterns/ProductionRule.java
@@ -8,19 +8,33 @@ import java.util.List;
  * Created by awatkins on 15-10-24.
  */
 public class ProductionRule {
+    private String _id;
     private String _name;
     private List<String> _symbols;
-    private boolean _isTopLevel;
     private boolean _isTerminal;
+
+    public String getName() {
+        return _name;
+    }
+    public List<String> getSymbols() {
+        return _symbols;
+    }
+    public boolean getIsTerminal() {
+        return _isTerminal;
+    }
+    public String getId() { return _id; }
 
     public ProductionRule(String name, String[] symbols) {
         this(name, Arrays.asList(symbols));
     }
 
     public ProductionRule(String name, List<String> symbols) {
-        _name = name;
+        _name = name.trim();
 
-        _symbols = new ArrayList<>(symbols);
+        _symbols = new ArrayList<>();
+        for(String currSymbol: symbols)
+            _symbols.add(currSymbol.trim());
+
         _isTerminal = true; //default
         for(String currSymbol: _symbols)
             if(!isSymbolTerminal(currSymbol))
@@ -28,29 +42,33 @@ public class ProductionRule {
                 _isTerminal = false;
                 break;
             }
+
+        _id = generateRuleId();
     }
 
-    public String getName() {
-        return _name;
-    }
 
-    public List<String> getSymbols() {
-        return _symbols;
-    }
-
-    public boolean getIsTerminal() {
-        return _isTerminal;
-    }
-
-    public void setIsTopLevel(boolean isTopLevel) {
-        _isTopLevel = isTopLevel;
-    }
-
-    public boolean getIsTopLevel() {
-        return _isTopLevel;
-    }
 
     public static boolean isSymbolTerminal(String symbol)
     { return symbol.contains("/");}
+
+    private String generateRuleId()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(_name.toLowerCase().trim());
+        sb.append(":");
+
+        boolean first = true;
+        for(String currSymbol: _symbols)
+        {
+            if(true == first)
+                first = false;
+            else
+                sb.append(",");
+
+            sb.append(currSymbol.toLowerCase().trim());
+        }
+
+        return sb.toString();
+    }
 
 }
