20ced7a24afb6ea2d5439d80aae4fd2870170230
Andrew Watkins <andrew.watkins@move.com>
2015-10-26 13:18:21 -0700

    updated rules

--- rezone.geocoder.parser/src/main/java/rezone/geocoder/parser/patterns/PatternManager.java
+++ rezone.geocoder.parser/src/main/java/rezone/geocoder/parser/patterns/PatternManager.java
@@ -14,12 +14,15 @@ public class PatternManager {
         s += "address :- address_line, city, state, zip;";
 
         s += "address_line :- street_number, street;";
+        s += "address_line :- street_number, street, unit;";
+
         s += "street :- street_name, street_suffix;";
         s += "street :- predirectional, street_name, street_suffix;";
 
         //terminal symbol
         s += "street_number :- street_no/1;";
         s += "street_name :- street/1;";
+        s += "street_name :- street/2;";
         s += "street_suffix :- street_suffix/1;";
         s += "predirectional :- directional/1;";
 
@@ -28,6 +31,7 @@ public class PatternManager {
         s += "state :- state/1;";
         s += "state :- state/2;";
         s += "zip  :- zip/1;";
+        s += "unit :- unit/2;";
 
         return s;
     }
